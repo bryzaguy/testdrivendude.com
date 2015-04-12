46c46679c90cd6e05be4bf8412d1684a334c87db
Bryant Syme <prefu@hotmail.com>
2015-04-12 14:10:33 -0700

    add server

---
+++ server.js
@@ -0,0 +1,11 @@
+/* jslint node: true */
+
+'use strict';
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end("Hello");
+}).listen(9615);
